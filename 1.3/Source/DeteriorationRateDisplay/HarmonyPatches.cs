eae3d2c8589145399c4e3c66b9d6b99319bd0e99
Evelyn <udderlyevelyn@gmail.com>
2021-12-14 15:25:45 -0500

    Fixed display.

--- 1.3/Source/DeteriorationRateDisplay/HarmonyPatches.cs
+++ 1.3/Source/DeteriorationRateDisplay/HarmonyPatches.cs
@@ -14,17 +14,16 @@ namespace DRD
 	[HarmonyPatch(typeof(Thing), "GetInspectStringLowPriority")]
     class HarmonyPatches
     {
+
 		static void Postfix(ref Thing __instance, ref string __result)
 		{
-			var finalDeteriorationRate = SteadyEnvironmentEffects.FinalDeteriorationRate(__instance);
 			float hpLossPerDay = 0;
+			float finalDeteriorationRate = SteadyEnvironmentEffects.FinalDeteriorationRate(__instance);
 			if (finalDeteriorationRate > .001) //If it's smaller then it takes no damage to leave it at 0.
 			{
-				//double deteriorationsPerDay = 12.37113402061856; //This was based on the %97==0 && rand.chance(.02) stuff but that was wrong.
 				var deteriorationRate = Mathf.Lerp(1f, 5f, __instance.Map.weatherManager.RainRate);
-				var deteriorationChance = deteriorationRate * finalDeteriorationRate / 36f;
-				hpLossPerDay = (float)(deteriorationChance * 60000); //60k here used to be deteriorationsPerDay
-				__result += "\nGlobal Deterioration Rate: " + deteriorationRate + "\nDeterioration Chance: " + deteriorationChance + "\nEstimated HP loss per day: " + Math.Round(hpLossPerDay, 2);
+				hpLossPerDay = deteriorationRate * finalDeteriorationRate;
+				__result += "\nDeterioration rate: " + Math.Round(hpLossPerDay, 2) + " / day";
 			}
 		}
 	}
